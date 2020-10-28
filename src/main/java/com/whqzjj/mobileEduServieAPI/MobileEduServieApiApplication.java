d69b5ca736cbebeb529c38700d361c03561c2f5a
strongthen <1010038964@qq.com>
2020-10-28 23:14:49 +0800

    武汉求知家教手机版后端服务代码

---
+++ src/main/java/com/whqzjj/mobileEduServieAPI/MobileEduServieApiApplication.java
@@ -0,0 +1,13 @@
+package com.whqzjj.mobileEduServieAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileEduServieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileEduServieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whqzjj/mobileEduServieAPI/MobileEduServieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whqzjj.mobileEduServieAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileEduServieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
